b166871300ad6d49a082b1b2a7a23a6aed27bc55
Ortiz Troncoso, Alvaro <alvaro.ortiztroncoso@mfn-berlin.de>
2017-10-30 12:04:22 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # emacs_python
-Configure emacs as python editor
+Configure emacs as python3 editor.
+
+GNU Emacs is a free, open source programming editor: https://www.gnu.org/software/emacs/
+
+This is a configuration file for using emacs as Python3 editor. It includes:
+* Styling
+* Python-mode, syntax checking, code style (pep8)
+* Directory navigation tree
+* Tabs for open files
+
+
+# Usage
+1. install GNU Emacs
+2. install flake8 and jedi using pip3
+3. put init.el in ~/.emacs.d
+4. restart emacs. After the first restart, emacs will download and install the necessary packages.
+
+# References
+Golubev, M. (2011) Emacs tabbar-mode visual tweaks. Online: https://gist.github.com/3demax/1264635 
+
+Purdon​, K. (2015) Emacs - the Best Python Editor? Online: https://realpython.com/blog/python/emacs-the-best-python-editor/
---
+++ init.el
@@ -0,0 +1,107 @@
+;; init.el --- Emacs configuration
+
+;; INSTALL PACKAGES
+(require 'package)
+
+(add-to-list 'package-archives
+       '("melpa" . "http://melpa.org/packages/") t)
+
+(package-initialize)
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(defvar myPackages
+  '(better-defaults
+    elpy
+    flycheck
+    py-autopep8
+    neotree
+    tabbar
+    material-theme))
+
+(mapc #'(lambda (package)
+    (unless (package-installed-p package)
+      (package-install package)))
+      myPackages)
+
+;; BASIC CUSTOMIZATION
+(setq inhibit-startup-message t) ;; hide the startup message
+(load-theme 'material t) ;; load material theme
+(global-linum-mode t) ;; enable line numbers globally
+
+;; PYTHON
+(setq py-python-command "/usr/bin/python3")
+(elpy-enable)
+(when (require 'flycheck nil t)
+  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+  (add-hook 'elpy-mode-hook 'flycheck-mode))
+(require 'py-autopep8)
+(add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
+
+;; DIRECTORY TREE
+(require 'neotree)
+(neotree-toggle)
+(neotree-dir "~/")
+
+;; FILE TABS
+(require 'tabbar)
+
+;; Tabbar settings copied from:
+;; Golubev, M. (2011) Emacs tabbar-mode visual tweaks. 
+;; https://gist.github.com/3demax/1264635
+
+;; Tabbar settings
+(set-face-attribute
+ 'tabbar-default nil
+ :background "gray20"
+ :foreground "gray20"
+ :box '(:line-width 1 :color "gray20" :style nil))
+(set-face-attribute
+ 'tabbar-unselected nil
+ :background "gray30"
+ :foreground "white"
+ :box '(:line-width 5 :color "gray30" :style nil))
+(set-face-attribute
+ 'tabbar-selected nil
+ :background "gray75"
+ :foreground "black"
+ :box '(:line-width 5 :color "gray75" :style nil))
+(set-face-attribute
+ 'tabbar-highlight nil
+ :background "white"
+ :foreground "black"
+ :underline nil
+ :box '(:line-width 5 :color "white" :style nil))
+(set-face-attribute
+ 'tabbar-button nil
+ :box '(:line-width 1 :color "gray20" :style nil))
+(set-face-attribute
+ 'tabbar-separator nil
+ :background "gray20"
+ :height 0.6)
+
+;; Change padding of the tabs
+;; we also need to set separator to avoid overlapping tabs by highlighted tabs
+(custom-set-variables
+ '(tabbar-separator (quote (0.5))))
+;; adding spaces
+(defun tabbar-buffer-tab-label (tab)
+  "Return a label for TAB.
+That is, a string used to represent it on the tab bar."
+  (let ((label  (if tabbar--buffer-show-groups
+                    (format "[%s]  " (tabbar-tab-tabset tab))
+                  (format "%s  " (tabbar-tab-value tab)))))
+    ;; Unless the tab bar auto scrolls to keep the selected tab
+    ;; visible, shorten the tab label to keep as many tabs as possible
+    ;; in the visible area of the tab bar.
+    (if tabbar-auto-scroll-flag
+        label
+      (tabbar-shorten
+       label (max 1 (/ (window-width)
+                       (length (tabbar-view
+                                (tabbar-current-tabset)))))))))
+
+(tabbar-mode 1)
+
+;; init.el ends here
+
